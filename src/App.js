138f95f63695c259d32126d381b3add6780ed1ac
pavel <zhpv07@gmail.com>
2020-12-15 14:45:40 +0600

    lesson51

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import Person from "./Person/Person";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        people: [
+            {name: 'Dmitrii', age: 28, about: "Hobby: Video Games"},
+            {name: 'John', age: 30, about: "Likes drinking alone"}
+        ]
+    };
+
+    changeName = () => {
+        const people = [...this.state.people];
+        const person = {...people[1]};
+        person.name = 'John Doe';
+        people[1] = person;
+        this.setState({people});
+    };
+
+    increaseAge = () => {
+        const people = this.state.people.map((person) => {
+            return {
+                ...person,
+                age: person.age + 1
+            };
+        });
+        this.setState({people});
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Person name={this.state.people[0].name} age={this.state.people[0].age}>
+                    {this.state.people[0].about}
+                </Person>
+                <Person name={this.state.people[1].name} age={this.state.people[1].age}>
+                    {this.state.people[1].about}
+                </Person>
+                <div><button onClick={this.changeName}>Change name</button></div>
+                <div><button onClick={this.increaseAge}>Increase Age</button></div>
+            </div>
+        );
+    }
 }
 
 export default App;
